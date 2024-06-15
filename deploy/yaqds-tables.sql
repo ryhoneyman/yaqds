ffc969061fec0f66d3c18b690ae8c226157f9534
ryhoneyman <ryhoneyman@gmail.com>
2024-06-15 10:56:47 +0000

    updated query

--- deploy/yaqds-tables.sql
+++ deploy/yaqds-tables.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS `yaqds_npc_loottable`;
+
 CREATE TABLE `yaqds_npc_loottable` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `nt_id` INT UNSIGNED NOT NULL,
@@ -14,3 +16,11 @@ CREATE TABLE `yaqds_npc_loottable` (
   PRIMARY KEY (`id`),
   UNIQUE (`nt_name`,`nt_loottable_id`,`s2_zone`)
 );
+
+INSERT INTO yaqds_npc_loottable (nt_id,nt_name,nt_level,nt_loottable_id,se_spawngroup_id,s2_id,s2_zone,s2_min_expansion,s2_max_expansion,se_min_expansion,se_max_expansion) 
+  SELECT nt.id,nt.name,nt.level,nt.loottable_id,se.spawngroupID,s2.id,s2.zone,s2.min_expansion,s2.max_expansion,se.min_expansion,se.max_expansion 
+  FROM npc_types nt 
+  LEFT JOIN spawnentry se ON nt.id = se.npcID 
+  LEFT JOIN spawn2 s2 ON se.spawngroupID = s2.spawngroupID 
+  WHERE nt.loottable_id > 0 AND s2.id IS NOT NULL
+  ON DUPLICATE KEY UPDATE nt_id = nt_id;
