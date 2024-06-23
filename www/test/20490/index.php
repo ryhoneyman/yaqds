14de5860cbcd786269c0e88e057428697a67d69f
ryhoneyman <ryhoneyman@gmail.com>
2024-06-23 13:17:54 +0000

    meta desc update

--- www/test/20490/index.php
+++ www/test/20490/index.php
@@ -2,7 +2,7 @@
 <head>
 <meta property="og:url" content="https://www.pqdi.cc/item/20490">
 <meta property="og:type" content="website">
-<meta property="og:title" content="Blood Orchid Katana :: Item :: PQDI">
+<meta property="og:title" content="Nature Walker's Scimitar :: Item :: PQDI">
 <meta property="og:description" content="MAGIC, LORE, NODROP
 PRIMARY 1HS 12/23
 HP+10 MANA+10 STR+15 STA+15 WIS+20
@@ -11,7 +11,7 @@ Effect: Wrath of Nature
 DRU
 ALL">
 <meta property="og:image" content="https://www.pqdi.cc/static/icons/item_1169.png">
-<title>Blood Orchid Katana :: Item :: PQDI</title> 
+<title>Nature Walker's Scimitar :: Item :: PQDI</title> 
 </head>
 <body>
 </body>
