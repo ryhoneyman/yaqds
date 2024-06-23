a37a248c98ad5bc8683d3d05f14172aad4fb083b
ryhoneyman <ryhoneyman@gmail.com>
2024-06-23 13:02:58 +0000

    testing meta props

---
+++ www/test/24636/index.php
@@ -0,0 +1,12 @@
+<>
+<head>
+<meta property="og:url" content="https://www.pqdi.cc/item/24636">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Blood Orchid Katana :: Item :: PQDI">
+<meta property="og:description" content="<table><tr><td>TestColA</td><td>TestColB</td></tr><tr><td>Val1</td><td>Val2</td></tr></table>">
+<meta property="og:image" content="https://www.pqdi.cc/static/icons/item_890.png">
+<title>Blood Orchid Katana :: Item :: PQDI</title> 
+</head>
+<body>
+</body>
+</html>
