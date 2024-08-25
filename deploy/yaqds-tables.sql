ede38e002f91ac742e22d8ecbe1c7e641b8fe61b
ryhoneyman <ryhoneyman@gmail.com>
2024-08-25 14:23:43 +0000

    updated table structure for new expansion values

--- deploy/yaqds-tables.sql
+++ deploy/yaqds-tables.sql
@@ -9,10 +9,10 @@ CREATE TABLE `yaqds_npc_loottable` (
   `se_spawngroup_id` INT UNSIGNED,
   `s2_id` INT UNSIGNED,
   `s2_zone` VARCHAR(32),
-  `s2_min_expansion` DECIMAL(2,1) DEFAULT 0,
-  `s2_max_expansion` DECIMAL(2,1) DEFAULT 0,
-  `se_min_expansion` DECIMAL(2,1) DEFAULT 0,
-  `se_max_expansion` DECIMAL(2,1) DEFAULT 0,
+  `s2_min_expansion` DECIMAL(3,1) DEFAULT 0,
+  `s2_max_expansion` DECIMAL(3,1) DEFAULT 0,
+  `se_min_expansion` DECIMAL(3,1) DEFAULT 0,
+  `se_max_expansion` DECIMAL(3,1) DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE (`nt_name`,`nt_loottable_id`,`s2_zone`)
 );
