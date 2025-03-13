087e1284e02b903d892281e0ad9c16be9efe0a74
ryhoneyman <ryhoneyman@gmail.com>
2025-03-13 18:26:19 +0000

    wip

--- www/r/index.php
+++ www/r/index.php
@@ -47,9 +47,9 @@ function routeItem($main)
     if (!preg_match('~/(\d+)$~',$requestUri,$match)) { $restapi->sendResponse(null,null,500,'html'); }
 
     $itemId   = $match[1];
-    $response = $api->v1Item($itemId);
+    $itemData = $api->v1Item($itemId);
 
-    if (!$response || $response['error']) { $restapi->sendResponse($response['error'] ?: null,null,404,'html'); }
+    if (!$itemData || $itemData['error']) { $restapi->sendResponse($itemData['error'] ?: null,null,404,'html'); }
 
     //print json_encode($response,JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
 
@@ -57,10 +57,17 @@ function routeItem($main)
 
     $values = [
         'URL'         => sprintf("%s%s",$baseUrl,$requestUri),
-        'TITLE'       => sprintf("%s | Item | YAQDS",$response['name']),
-        'DESCRIPTION' => implode("\n",$response['_description']),
-        'IMAGE'       => sprintf("%s/images/icons/item_%d.png",$baseUrl,$response['icon']),
+        'TITLE'       => sprintf("%s | Item | YAQDS",$itemData['name']),
+        'DESCRIPTION' => implode("\n",$itemData['_description']),
+        'IMAGE'       => sprintf("%s/images/icons/item_%d.png",$baseUrl,$itemData['icon']),
     ];
+
+    $values['BODY'] = "<div style='background:black; color:white; font-family:arial; padding:5px; width:min-content;'>\n". 
+                      "<div style='background:black; color:#bbb; font-family:arial; border:1px solid #333; padding:5px; width:auto; min-width:400px; text-align:center;'>".$itemData['name']."</div>\n". 
+                      "<div style='padding:2px;'></div>\n".
+                      "<div style='background:black; color:white; font-family:arial; border:1px solid #333; white-space:pre; padding:10px; width:auto; min-width:400px;'>".
+                      "<img src='".$values['IMAGE']."' style='float:right; height:auto;'>".$values['DESCRIPTION']."</div>\n". 
+                      "</div>";
 
     $template = "<html><head>\n". 
                 "<meta property='og:url' content='{{URL}}'>\n".
@@ -68,7 +75,9 @@ function routeItem($main)
                 "<meta property='og:title' content='{{TITLE}}'>\n".
                 "<meta property='og:description' content='{{DESCRIPTION}}'>\n".
                 "<meta property='og:image' content='{{IMAGE}}'>\n". 
-                "</head></html>";
+                "</head>\n".
+                "<body>{{BODY}}</body>\n".
+                "</html>";
 
     print replaceValues($template,$values);
 }
