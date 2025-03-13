bfa3d2cbcd0e8d29f685a6d5e75a9ab942beb0fd
ryhoneyman <ryhoneyman@gmail.com>
2025-03-13 16:40:53 +0000

    initial commit

---
+++ lib/local/myapi.class.php
@@ -0,0 +1,146 @@
+<?php
+
+include_once 'common/apibase.class.php';
+
+class MyAPI extends LWPLib\APIBase
+{
+   protected $version  = 1.0;
+   
+   /**
+    * __construct
+    *
+    * @param  LWPLib\Debug|null $debug
+    * @param  array|null $options
+    * @return void
+    */
+    public function __construct($debug = null, $options = null)
+    {
+        parent::__construct($debug,$options);
+
+        $this->authType('auth.header.bearer');
+
+        $this->loadUris([
+            'v1-authenticate'              => '/v1/auth/token',
+            'v1-data-provider-query'       => '/v1/data/provider/query/{{database}}',
+            'v1-data-provider-query-table' => '/v1/data/provider/query/{{database}}/{{table}}',
+            'v1-data-provider-modify'      => '/v1/data/provider/modify/{{database}}',
+            'v1-item'                      => '/v1/item/{{id}}',
+            'v1-item-description'          => '/v1/item/description/{{id}}',
+        ]);
+    }
+    
+    /**
+     * v1ItemDescription
+     *
+     * @param  int $itemId
+     * @return bool|array
+     */
+    public function v1Item(int $itemId)
+    {
+        $request = [
+            'params' => ['id' => $itemId],
+            'options' => [
+                'method' => 'GET',
+            ],
+        ];
+ 
+        if (!$this->makeRequest('v1-item','auth,json',$request)) { 
+            $this->error($this->clientError());
+            return false; 
+        }
+    
+        return $this->clientResponse();
+    } 
+           
+    /**
+        * v1DataProviderBindQuery
+        *
+        * @param  string $database
+        * @param  string $statement
+        * @param  string|null $types
+        * @param  array|null $data
+        * @param  array|null $options
+        * @return mixed
+        */
+    public function v1DataProviderBindQuery($database, $statement, $types = null, $data = null, $options = null)
+    {
+        $single = $options['single'] ?: false;
+
+        $request = [
+            'params' => ['database' => $database],
+            'data' => [
+                'statement' => $statement, 
+                'types'     => $types,
+                'data'      => $data,
+                'single'    => $single
+            ],
+            'options' => [
+                'method' => 'POST',
+            ]
+        ];
+
+        if (!$this->makeRequest('v1-data-provider-query','auth,json',$request)) { 
+            $this->error($this->clientError());
+            return false; 
+        }
+
+        return $this->clientResponse();
+    }
+
+    /**
+        * v1DataProviderTableData
+        *
+        * @param  string $database
+        * @param  string $table
+        * @param  array|null $options
+        * @return mixed
+        */
+    public function v1DataProviderTableData($database, $table, $options = null)
+    {
+        $options = (is_array($options)) ? $options : [];
+
+        $request = [
+            'params' => ['database' => $database, 'table' => $table],
+            'data' => $options,
+            'options' => [
+                'method' => 'POST',
+            ]
+        ];
+
+        if (!$this->makeRequest('v1-data-provider-query-table','auth,json',$request)) { 
+            $this->error($this->clientError());
+            return false; 
+        }
+
+        return $this->clientResponse();
+    }
+
+    /**
+    * v1Authenticate
+    *
+    * @param  string $clientId
+    * @param  string $clientSecret
+    * @return bool
+    */
+    public function v1Authenticate($clientId, $clientSecret)
+    {
+        $request = [
+            'data' => ['client_id' => $clientId, 'client_secret' => $clientSecret],
+            'options' => [
+                'timeout' => 15,
+            ],
+        ];
+
+        $requestResult = $this->makeRequest('v1-authenticate','json',$request);
+
+        if ($requestResult === false) { $this->error($this->clientError()); return false; }
+
+        $token = $this->clientResponseValue('token');
+
+        if (!$token) { $this->error('Could not authenticate'); return false; }
+
+        $this->authToken = $token;
+
+        return true;
+    } 
+}
---
+++ www/r/index.php
@@ -0,0 +1,60 @@
+<?php
+include_once 'yaqds-init.php';
+include_once 'local/main.class.php';
+
+$main = new Main([
+    'debugLevel'     => 0,
+    'debugType'      => DEBUG_COMMENT,
+    'debugBuffer'    => false,
+    'debugLogDir'    => APP_LOGDIR,
+    'errorReporting' => false, 
+    'sessionStart'   => true,
+    'memoryLimit'    => null,
+    'sendHeaders'    => true,
+    'database'       => false,
+    'dbConfigDir'    => APP_CONFIGDIR,
+    'fileDefines'    => null,
+    'dbDefines'      => null,
+    'input'          => false,
+    'html'           => false,
+    'adminlte'       => false,
+]);
+
+$settings   = json_decode(file_get_contents(APP_CONFIGDIR.'/settings.json'),true);
+$apiOptions = ['baseUrl' => $settings['YAQDS_API_URL'], 'authToken' => $settings['YAQDS_API_AUTH_TOKEN']];
+
+if (!$main->buildClass('api','MyAPI',$apiOptions,'local/myapi.class.php')) { exit; }
+if (!$main->buildClass('restapi','LWPLib\RESTAPI',null,'restapi.class.php')) { exit; }
+
+//$main->prepareDatabase('db.yaqds.conf','yaqds');
+
+$restapi = $main->obj('restapi');
+
+$restapi->router($main,[
+    '/r/item/' => ['function' => 'routeItem', 'method' => ['GET']],
+]);
+ 
+?>
+<?php
+
+function routeItem($main)
+{
+    $api     = $main->obj('api');
+    $restapi = $main->obj('restapi');
+
+    $requestUri = $restapi->requestUri();
+
+    if (!preg_match('~/(\d+)$~',$requestUri,$match)) { $restapi->sendResponse(null,null,500,'html'); }
+
+    $itemId   = $match[1];
+    $response = $api->v1Item($itemId);
+
+    if (!$response || $response['error']) { $restapi->sendResponse($response['error'] ?: null,null,404,'html'); }
+
+    $itemData = $response['data'];
+    $itemDesc = $response['description'];
+
+    print json_encode($itemDesc,JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+}
+
+?>
