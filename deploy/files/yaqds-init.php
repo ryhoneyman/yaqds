e4bf0ddd2016eb9dcaf42e63f92e380404d0ed9d
ryhoneyman <ryhoneyman@gmail.com>
2025-05-12 12:49:53 +0000

    update cache dir

--- deploy/files/yaqds-init.php
+++ deploy/files/yaqds-init.php
@@ -1,13 +1,12 @@
 <?php
-
 define('APP_BASEDIR','/opt/yaqds');
 #define('APP_BASEDIR','/home/u690380958/domains/yaqds.cc');  ### Hostinger
 define('APP_LIBDIR',APP_BASEDIR.'/lib');
 define('APP_WEBDIR',APP_BASEDIR.'/www');
-define('APP_CACHEDIR',APP_BASEDIR.'/cache');   // dynamic cache
 define('APP_CONFIGDIR',APP_BASEDIR.'/etc');    // static configurations
 define('APP_DATADIR',APP_BASEDIR.'/data');     // static private file data
 define('APP_VARDIR',APP_BASEDIR.'/var');       // dynamic file data
+define('APP_CACHEDIR',APP_VARDIR.'/cache');   // dynamic cache
 define('APP_LOGDIR',APP_BASEDIR.'/log');       // logs
 define('APP_LOCALDIR',APP_BASEDIR.'/local');   // scripts for cron, daemons, scheduler
 
